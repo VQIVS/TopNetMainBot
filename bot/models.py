ca33ba70e75cc6652807b93addad0d77b8576bc0
AmirMahdi Qiasvand <amirmahdiqiasvand@gmail.com>
2023-08-29 19:54:53 +0330

    Update models.py
    crate the bot app models

--- bot/models.py
+++ bot/models.py
@@ -1,3 +1,44 @@
 from django.db import models
 
-# Create your models here.
+
+class User(models.Model):
+    user_id = models.CharField(max_length=255)
+    email = models.EmailField(max_length=255)
+
+    def __str__(self):
+        return self.email
+
+
+class Products(models.Model):
+    product_id = models.IntegerField(max_length=10)
+    link = models.CharField(max_length=255)
+    price = models.IntegerField(max_length=255)
+
+
+class Order(models.Model):
+    user_id = models.ForeignKey(
+        "User",
+        on_delete=models.CASCADE,
+    )
+
+    product_id = models.ForeignKey(
+        "Products",
+        on_delete=models.CASCADE
+    )
+
+    status = models.CharField(max_length=255)
+    quantity = models.IntegerField()
+
+
+class Purchase(models.Model):
+    user_id = models.ForeignKey(
+        "User",
+        on_delete=models.CASCADE,
+    )
+
+    product_id = models.ForeignKey(
+        "Products",
+        on_delete=models.CASCADE
+    )
+
+    status = models.CharField(max_length=255)
