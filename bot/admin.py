21984c3ec9117a3383ded8a2ee3572d8fd4becb0
AmirMahdi Qiasvand <amirmahdiqiasvand@gmail.com>
2023-08-29 21:38:59 +0330

    Update admin.py
    add purchase id col to purchase table

--- bot/admin.py
+++ bot/admin.py
@@ -2,4 +2,11 @@ from django.contrib import admin
 from .models import *
 
 
-admin.site.register(User, Products, Order, Purchase)
+admin.site.register(User)
+admin.site.register(Purchase)
+admin.site.register(Order)
+admin.site.register(Product)
+
+class Admin(admin.ModelAdmin):
+    login_required = False
+
