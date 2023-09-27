1e5450665991b584635d87b0469863c63a551a02
AmirMahdi Qiasvand <amirmahdiqiasvand@gmail.com>
2023-09-27 21:26:41 +0330

    Update views.py
    creating a viewset for User model

--- bot/api/v1/views.py
+++ bot/api/v1/views.py
@@ -1,28 +1,18 @@
-from rest_framework.decorators import api_view
-from rest_framework.response import Response
-from .serializers import LinkSerializer
-from ...models import Link
+from .serializers import LinkSerializer, UserSerializer
+from ...models import Link, User
 from rest_framework.viewsets import ModelViewSet
-
-
-# @api_view(['GET', 'POST'])
-# def post_list(request):
-#     if request.method == "GET":
-#         links = Link.objects.all()
-#         serializer = LinkSerializer(links, many=True)
-#         data = serializer.data
-#         return Response(data)
-#     elif request.method == 'POST':
-#         serializer = LinkSerializer(data=request.data)
-#         if serializer.is_valid():
-#             serializer.save()
-#             print("Link saved successfully")
-#             return Response(serializer.data)
-#         else:
-#             print("Serializer errors:", serializer.errors)
-#             return Response(serializer.errors)
 
 
 class ModelViewSetLinks(ModelViewSet):
     serializer_class = LinkSerializer
     queryset = Link.objects.all()
+
+class ModelViewSetUser(ModelViewSet):
+    serializer_class = UserSerializer
+    queryset = User.objects.all()
+
+
+
+
+
+
