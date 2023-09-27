9db5dbb68dd7f16fa8221ef7c8e33032d75355cb
AmirMahdi Qiasvand <amirmahdiqiasvand@gmail.com>
2023-09-27 20:30:00 +0330

    Api new update

--- bot/api/v1/urls.py
+++ bot/api/v1/urls.py
@@ -1,7 +1,12 @@
-from django.urls import path
-from .views import post_list
+from django.urls import path, include
+from .views import ModelViewSetLinks
+from rest_framework.routers import DefaultRouter
+
+router = DefaultRouter()
+router.register('links', ModelViewSetLinks, basename="links-api")
 app_name = "api-v1"
 
 urlpatterns = [
-    path('post_list/', post_list, name='post_list')
+    # path('post_list/', post_list, name='post_list')
+    path('', include(router.urls))
 ]
--- bot/api/v1/views.py
+++ bot/api/v1/views.py
@@ -2,20 +2,27 @@ from rest_framework.decorators import api_view
 from rest_framework.response import Response
 from .serializers import LinkSerializer
 from ...models import Link
+from rest_framework.viewsets import ModelViewSet
 
-@api_view(['GET', 'POST'])
-def post_list(request):
-    if request.method == "GET":
-        links = Link.objects.all()
-        serializer = LinkSerializer(links, many=True)
-        data = serializer.data
-        return Response(data)
-    elif request.method == 'POST':
-        serializer = LinkSerializer(data=request.data)
-        if serializer.is_valid():
-            serializer.save()
-            print("Link saved successfully")
-            return Response(serializer.data)
-        else:
-            print("Serializer errors:", serializer.errors)
-            return Response(serializer.errors)
+
+# @api_view(['GET', 'POST'])
+# def post_list(request):
+#     if request.method == "GET":
+#         links = Link.objects.all()
+#         serializer = LinkSerializer(links, many=True)
+#         data = serializer.data
+#         return Response(data)
+#     elif request.method == 'POST':
+#         serializer = LinkSerializer(data=request.data)
+#         if serializer.is_valid():
+#             serializer.save()
+#             print("Link saved successfully")
+#             return Response(serializer.data)
+#         else:
+#             print("Serializer errors:", serializer.errors)
+#             return Response(serializer.errors)
+
+
+class ModelViewSetLinks(ModelViewSet):
+    serializer_class = LinkSerializer
+    queryset = Link.objects.all()
