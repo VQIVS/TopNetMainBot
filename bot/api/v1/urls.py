a28eb42d0014be2d872c8ffa130cdea36dd596df
AmirMahdi Qiasvand <amirmahdiqiasvand@gmail.com>
2023-09-27 21:26:16 +0330

    Update urls.py

--- bot/api/v1/urls.py
+++ bot/api/v1/urls.py
@@ -1,9 +1,10 @@
 from django.urls import path, include
-from .views import ModelViewSetLinks
+from .views import ModelViewSetLinks, ModelViewSetUser
 from rest_framework.routers import DefaultRouter
 
 router = DefaultRouter()
 router.register('links', ModelViewSetLinks, basename="links-api")
+router.register('User', ModelViewSetUser, basename="Users-api")
 app_name = "api-v1"
 
 urlpatterns = [
