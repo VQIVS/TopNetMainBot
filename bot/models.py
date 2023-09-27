c289767a561cd60141635c066ee0e02877fee55c
AmirMahdi Qiasvand <amirmahdiqiasvand@gmail.com>
2023-09-27 20:32:56 +0330

    Update models.py
    edit the email field to be unique.

--- bot/models.py
+++ bot/models.py
@@ -14,7 +14,7 @@ class Link(models.Model):
 
 class User(models.Model):
     user_id = models.CharField(max_length=255)
-    email = models.EmailField(max_length=255)
+    email = models.EmailField(max_length=255, unique=True)
 
     def __str__(self):
         return self.email
