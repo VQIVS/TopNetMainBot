043445f1d698f14729cd1ca000e489809ff6645e
AmirMahdi Qiasvand <amirmahdiqiasvand@gmail.com>
2023-09-27 21:26:12 +0330

    Update serializers.py
    I have created a serializer for User api.

--- bot/api/v1/serializers.py
+++ bot/api/v1/serializers.py
@@ -1,8 +1,13 @@
 # serializers.py
 from rest_framework import serializers
-from ...models import Link
+from ...models import Link, User
 
 class LinkSerializer(serializers.ModelSerializer):
     class Meta:
         model = Link
         fields = ['link_id', 'link', 'price']
+
+class UserSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = User
+        fields = ['user_id', 'email']
