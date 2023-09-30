80c726d9f826142bf90a24f899a9d757bbab19fe
AmirMahdi Qiasvand <amirmahdiqiasvand@gmail.com>
2023-09-30 20:50:39 +0330

    Update keyboards.py
    create some keyboards

--- bot/keyboards.py
+++ bot/keyboards.py
@@ -12,7 +12,7 @@ buttons_row2 = [
 ]
 buttons_row3 = [
     types.KeyboardButton('📚راهنما اتصال'),
-    types.KeyboardButton('سوالات متداول'),
+    types.KeyboardButton('اضافه کردن ایمیل'),
 ]
 buttons_row4 = [
     types.KeyboardButton('🛍 خرید عمده'),
@@ -27,11 +27,13 @@ keyboard.add(*buttons_row4)
 # FAQZ_keyboard.add(button)
 
 products_keyboard = types.InlineKeyboardMarkup(row_width=1)
-product_1 = types.InlineKeyboardButton('⚪️ نقره‌ای 1 : یک ماهه 25GB', callback_data='product_1')
-product_2 = types.InlineKeyboardButton('⚪️ نقره‌ای 2 : یک ماهه 50GB', callback_data='product_2')
-product_3 = types.InlineKeyboardButton('⚪️ نقره‌ای 3 : یک ماهه 75GB', callback_data='product_3')
-product_4 = types.InlineKeyboardButton('🟡 طلایی 4 : یک ماهه 100GB', callback_data='product_4')
-products_keyboard.add(product_1, product_2, product_3, product_4)
+product_1 = types.InlineKeyboardButton('1 کاربر |📆 مدت: 30 روز |‌🔵 25 گیگابایت |‌ 79 هزار تومان (19ترون)', callback_data='⭐️ گروه SILVER')
+product_2 = types.InlineKeyboardButton('1 کاربر |📆 مدت: 30 روز |‌🔵 50 گیگابایت |‌ 119 هزار تومان (29ترون)', callback_data='⭐️ گروه GOLD-1')
+product_3 = types.InlineKeyboardButton('2 کاربر |📆 مدت: 30 روز |‌🔵 50 گیگابایت |‌ 139 هزار تومان (34ترون)', callback_data='⭐️ گروه GOLD-2')
+product_4 = types.InlineKeyboardButton('1 کاربر |📆 مدت: 30 روز |‌🔵 75 گیگابایت |‌ 159 هزار تومان (38ترون)', callback_data='⭐️ گروه PLATINUM-1')
+product_5 = types.InlineKeyboardButton('3 کاربر |📆 مدت: 30 روز |‌🔵 75 گیگابایت |‌ 179 هزار تومان (43ترون)', callback_data='⭐️ گروه PLATINUM-2')
+product_6 = types.InlineKeyboardButton('4 کاربر |📆 مدت: 30 روز |‌🔵 100 گیگابایت |‌ 199 هزار تومان (48ترون)', callback_data='⭐️ گروه DIAMOND')
+products_keyboard.add(product_1, product_2, product_3, product_4, product_5, product_6)
 
 ConfirmOrder_keyboard = types.InlineKeyboardMarkup(row_width=2)
 yes_btn = types.InlineKeyboardButton("بله", callback_data="بله")
@@ -39,8 +41,14 @@ no_btn = types.InlineKeyboardButton("لفو", callback_data="لفو")
 ConfirmOrder_keyboard.add(yes_btn, no_btn)
 
 payment_keyboard = types.InlineKeyboardMarkup(row_width=1)
-pay_button = types.InlineKeyboardButton('پرداخت', callback_data="pay")
-payment_keyboard.add(pay_button)
+pay_button = types.InlineKeyboardButton('پرداخت', callback_data="pay", url="https://t.me/Feenonetbot")
+how_to_pay_button = types.InlineKeyboardButton('نحوه پرداخت', callback_data="pay_btn")
+payment_keyboard.add(pay_button, how_to_pay_button)
+
+
+
+
+
 
 
 
