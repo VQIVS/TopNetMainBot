4132988057a73a04fdef022d4c7732d79d586d09
AmirMahdi Qiasvand <amirmahdiqiasvand@gmail.com>
2023-09-30 20:46:54 +0330

    Update models.py
    create Email class to allow user input email address
    
    some bug fixes and  add created_at fields to the models
    
    delete Purchase class

--- bot/models.py
+++ bot/models.py
@@ -1,9 +1,10 @@
-from django.db import models
 from django.contrib import admin
+from django.db import models
+import datetime
 
 
 class Link(models.Model):
-    link_id = models.IntegerField()
+    link_id = models.CharField(max_length=255, default="none")
     link = models.CharField(max_length=255)
     price = models.IntegerField()
     status = models.BooleanField(default=True)
@@ -12,44 +13,38 @@ class Link(models.Model):
         return self.link
 
 
-class User(models.Model):
-    user_id = models.CharField(max_length=255)
-    email = models.EmailField(max_length=255, unique=True)
+class Email(models.Model):
+    address = models.EmailField(max_length=255, unique=True)
 
     def __str__(self):
-        return self.email
+        return self.address
+
+
+class User(models.Model):
+    user_id = models.IntegerField()
+    primary_email = models.EmailField(max_length=255, unique=True, blank=True, default="12@gmal.com")
+    emails = models.ManyToManyField('Email', blank=True)
+    created_at = models.DateTimeField(default=datetime.datetime.now, blank=True, null=True)
+
+    def __str__(self):
+        return str(self.user_id)
 
 
 class Order(models.Model):
-    user_id = models.ForeignKey(
-        "User",
-        on_delete=models.CASCADE,
-    )
-
-    link_id = models.ForeignKey(
-        "Link",
-        on_delete=models.CASCADE
-    )
-
+    user_id = models.ForeignKey(User, on_delete=models.CASCADE, related_name="orders_by_user")
+    username = models.CharField(max_length=255, default="name")
+    link_id = models.IntegerField()
+    email = models.ForeignKey(Email, on_delete=models.CASCADE, related_name="orders_by_email")
     status = models.CharField(max_length=255)
     quantity = models.IntegerField()
+    created_at = models.DateTimeField(default=datetime.datetime.now, blank=True, null=True)
 
-
-class Purchase(models.Model):
-    user_id = models.ForeignKey(
-        "User",
-        on_delete=models.CASCADE,
-    )
-
-    purchase_id = models.CharField(max_length=255, default="none")
-
-    link_id = models.ForeignKey(
-        "Link",
-        on_delete=models.CASCADE
-    )
-
-#     status = models.CharField(max_length=255)
+    def __str__(self):
+        return str(self.user_id)
 
 
 class Admin(admin.ModelAdmin):
     login_required = True
+
+
+
