9dbad0901ed789f0e8685a7cdf34802961dc3783
AmirMahdi Qiasvand <amirmahdiqiasvand@gmail.com>
2023-09-30 20:49:39 +0330

    Update admin.py
    add Email

--- bot/admin.py
+++ bot/admin.py
@@ -5,6 +5,7 @@ admin.site.register(User)
 admin.site.register(Purchase)
 admin.site.register(Order)
 admin.site.register(Link)
+admin.site.register(Email)
 
 class Admin(admin.ModelAdmin):
     login_required = False
