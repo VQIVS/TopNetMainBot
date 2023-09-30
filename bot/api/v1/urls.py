a51a91541df724e83a96b58517e625a2722714ea
AmirMahdi Qiasvand <amirmahdiqiasvand@gmail.com>
2023-09-30 20:51:24 +0330

    Update urls.py
    add to router urls ModelViewSetUser, ModelViewSetUser, ModelViewSetOrder, ModelViewSetEmail

--- bot/api/v1/urls.py
+++ bot/api/v1/urls.py
@@ -1,10 +1,14 @@
 from django.urls import path, include
 from .views import ModelViewSetLinks, ModelViewSetUser
+from .views import ModelViewSetLinks, ModelViewSetUser, ModelViewSetOrder, ModelViewSetEmail
 from rest_framework.routers import DefaultRouter
 
 router = DefaultRouter()
 router.register('links', ModelViewSetLinks, basename="links-api")
-router.register('User', ModelViewSetUser, basename="Users-api")
+router.register('Users', ModelViewSetUser, basename="Users-api")
+router.register('Orders', ModelViewSetOrder, basename="Orders-api")
+router.register('Emails', ModelViewSetEmail, basename="Emails-api")
+
 app_name = "api-v1"
 
 urlpatterns = [
