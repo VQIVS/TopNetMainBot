cb9e83ffb42134dff10733935fb8c5dafe433750
AmirMahdi Qiasvand <amirmahdiqiasvand@gmail.com>
2023-09-30 20:50:26 +0330

    Update serializers.py
    create OrderSerializer, EmailSerializer

--- bot/api/v1/serializers.py
+++ bot/api/v1/serializers.py
@@ -1,6 +1,6 @@
 # serializers.py
 from rest_framework import serializers
-from ...models import Link, User
+from ...models import Link, User, Order, Email
 
 class LinkSerializer(serializers.ModelSerializer):
     class Meta:
@@ -10,4 +10,21 @@ class LinkSerializer(serializers.ModelSerializer):
 class UserSerializer(serializers.ModelSerializer):
     class Meta:
         model = User
-        fields = ['user_id', 'email']
+        fields = ['user_id', 'primary_email', 'emails', 'created_at']
+
+class OrderSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Order
+        fields = ['user_id', "username", 'link_id', 'email', 'status', 'quantity', 'created_at']
+
+class EmailSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Email
+        fields = ['address']
+
+
+
+
+
+
+
