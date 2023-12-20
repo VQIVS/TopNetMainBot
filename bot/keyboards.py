23b360fbe46439af30238ab8218f2fa29759585b
AmirMahdi Qiasvand <amirmahdiqiasvand@gmail.com>
2023-12-20 17:40:28 +0330

    Update keyboards.py
    some keyboard edits

--- bot/keyboards.py
+++ bot/keyboards.py
@@ -1,5 +1,4 @@
 from telebot import types
-from telebot.types import InlineKeyboardMarkup
 
 keyboard = types.ReplyKeyboardMarkup(resize_keyboard=True)
 buttons_row1 = [
@@ -18,12 +17,12 @@ keyboard.add(*buttons_row2)
 keyboard.add(*buttons_row3)
 
 products_keyboard = types.InlineKeyboardMarkup(row_width=1)
-product_1 = types.InlineKeyboardButton('1 کاربر |📆 مدت: 30 روز |‌🔵 30 گیگابایت |‌ 79 هزار تومان', callback_data='⭐️ گروه SILVER')
+product_1 = types.InlineKeyboardButton('2 کاربر |📆 مدت: 30 روز |‌🔵 30 گیگابایت |‌ 79 هزار تومان', callback_data='⭐️ گروه SILVER')
 product_2 = types.InlineKeyboardButton('1 کاربر |📆 مدت: 30 روز |‌🔵 60 گیگابایت |‌ 119 هزار تومان', callback_data='⭐️ گروه GOLD-1')
 product_3 = types.InlineKeyboardButton('2 کاربر |📆 مدت: 30 روز |‌🔵 60 گیگابایت |‌ 139 هزار تومان', callback_data='⭐️ گروه GOLD-2')
-product_4 = types.InlineKeyboardButton('3 کاربر |📆 مدت: 30 روز |‌🔵 90 گیگابایت |‌ 159 هزار تومان', callback_data='⭐️ گروه PLATINUM-1')
-product_5 = types.InlineKeyboardButton('4 کاربر |📆 مدت: 30 روز |‌🔵 120 گیگابایت |‌ 199 هزار تومان', callback_data='⭐️ گروه PLATINUM-2')
-product_6 = types.InlineKeyboardButton('1 کاربر |📆 مدت: 30 روز |‌🔵 حجم نامحدود  |‌ 299 هزار تومان', callback_data='⭐️ گروه DIAMOND')
+product_4 = types.InlineKeyboardButton('1 کاربر |📆 مدت: 30 روز |‌🔵 90 گیگابایت |‌ 159 هزار تومان', callback_data='⭐️ گروه PLATINUM-1')
+product_5 = types.InlineKeyboardButton('3 کاربر |📆 مدت: 30 روز |‌🔵 90 گیگابایت |‌ 179 هزار تومان', callback_data='⭐️ گروه PLATINUM-2')
+product_6 = types.InlineKeyboardButton('4 کاربر |📆 مدت: 30 روز |‌🔵 120 گیگابایت  |‌ 199 هزار تومان', callback_data='⭐️ گروه DIAMOND')
 products_keyboard.add(product_1, product_2, product_3, product_4, product_5, product_6)
 
 ConfirmOrder_keyboard = types.InlineKeyboardMarkup(row_width=2)
