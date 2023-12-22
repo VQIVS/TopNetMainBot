04d765442f2ee12b8235b0b6b21b9c1e7ddb42ad
AmirMahdi Qiasvand <amirmahdiqiasvand@gmail.com>
2023-12-22 18:42:35 +0330

    Update keyboards.py
    fixes on purchase keyboard

--- bot/keyboards.py
+++ bot/keyboards.py
@@ -37,10 +37,10 @@ no_btn = types.InlineKeyboardButton("لفو", callback_data="لفو")
 ConfirmOrder_keyboard.add(yes_btn, no_btn)
 
 payment_keyboard = types.InlineKeyboardMarkup(row_width=1)
-pay_button = types.InlineKeyboardButton('پرداخت با ترون', callback_data="pay", url="https://t.me/Feenonetbot")
+# pay_button = types.InlineKeyboardButton('پرداخت با ترون', callback_data="pay", url="https://t.me/Feenonetbot")
 pay_card = types.InlineKeyboardButton('شماره کارت', callback_data="pay_cart")
-how_to_pay_button = types.InlineKeyboardButton('نحوه پرداخت', callback_data="pay_btn", url="https://t.me/PardakhtFinonet/3")
-payment_keyboard.add(pay_button, pay_card, how_to_pay_button)
+# how_to_pay_button = types.InlineKeyboardButton('نحوه پرداخت', callback_data="pay_btn", url="https://t.me/PardakhtFinonet/3")
+payment_keyboard.add(pay_card)
 
 
 
